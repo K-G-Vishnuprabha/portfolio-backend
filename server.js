e3fad2ee39f30cdc053c7eb6824e4d7c2dd28d49
K-G-Vishnuprabha <vp194414@gmail.com>
2026-06-15 19:06:22 +0530

    Update CORS origin to production URLfinal

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://portfolio-frontend-eight-jade.vercel.app/',
     methods: ['GET', 'POST'],
   })
 );
